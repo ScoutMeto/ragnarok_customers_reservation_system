f35d12c745a9d58edea48e3d49955820bc2fa97d
Matej <m.pella@seznam.cz>
2025-08-14 00:52:21 +0200

    poslední fungující verze, vše hotovo (funguje na postgresql)

--- src/main/java/com/matejmarek/ragnarok_customers_reservation_system/service/TrainingServiceImpl.java
+++ src/main/java/com/matejmarek/ragnarok_customers_reservation_system/service/TrainingServiceImpl.java
@@ -102,6 +102,7 @@ public class TrainingServiceImpl implements TrainingService {
     // Data pro kalendář
     @Override
 //    @Cacheable(value = "trainingsByMonth", key = "#startDate.toLocalDate().toString() + '-' + #endDate.toLocalDate().toString()")
+//    @Cacheable("trainingsByMonth")
     @Cacheable("trainingsByMonth")
     public List<TrainingResponseDTO> getAllTrainingsAsCalendarEvents(LocalDateTime startDate, LocalDateTime endDate) {
         List<TrainingEntity> trainings = trainingRepository.findByDateOfCurrentLessonBetween(startDate, endDate);
